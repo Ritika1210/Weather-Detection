a1f6226550f3cf6999f040aa663fc0f1839cef33
Ritika1210 <ritikayadav68087@gmail.com>
2026-02-08 16:04:24 +0530

    initial weather app project

---
+++ project1.js
@@ -0,0 +1,21 @@
+const btn= document.getElementById("search-btn");
+const ip= document.getElementById("city-ip")
+const cityname = document.getElementById("city-name");
+const cityTime = document.getElementById("city-time");
+const temp = document.getElementById("temp");
+const API_KEY = "cccd4cefcabb4582bb9125729250110";
+async function getdata(cityName) {
+   const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=no`)
+    return await promise.json();
+    
+}
+btn.addEventListener("click",async ()=>
+{
+    const val = ip.value;
+    const res = await getdata(val);
+    console.log(res);
+   cityname.innerText = `${res.location.name}, ${res.location.region}, ${res.location.country}`;
+   cityTime.innerText = res.location.localtime;
+   temp.innerText = res.current.temp_c;
+
+})
